a7d13e2ca125a5b17432ed2f21d76239b3d59a6a
ebraheemnas <ebraheem.nas@gmail.com>
2022-07-20 22:10:15 +0200

    3

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -20,27 +20,34 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
             <div className="flex flex-col items-center w-full mt-3">
                 <div className="display-flex justify-start w-full mb-6 p-2">
 
-                <a href= {`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
-                    <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p> {/* i neeed to change the ETH network from ropsten to goerli*/}
-                </a>
+                    <a href= {`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+                        <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p> {/* i neeed to change the ETH network from ropsten to goerli*/}
+                    </a>
 
-                <a href= {`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
-                    <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
-                </a> 
-                    <p className="text-white text-base">Amount: {amount} ETH</p>
+                    <a href= {`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+                        <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
+                    </a> 
+                        <p className="text-white text-base">Amount: {amount} ETH</p>
 
-                    {message && (
-                        <>
-                        <br />
-                        <p className="text-white text-base">Message: {message}</p>
-                        </>
-                    )}
+                        {message && (
+                            <>
+                            <br />
+                            <p className="text-white text-base">Message: {message}</p>
+                            </>
+                        )}
+                </div>
+
+                    <img
+                        src={gifUrl || url}
+                        alt="gif"
+                        className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"/>
+                    
 
                     <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
                         <p className="text-[#37c7da] font-bold">{timestamp}</p>
                     </div>
 
-                </div>
+                
 
             </div>
 
