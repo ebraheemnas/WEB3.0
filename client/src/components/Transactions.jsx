d8824c8bbd365d416fdf2ee736cee61d8347b867
ebraheemnas <ebraheem.nas@gmail.com>
2022-07-20 19:44:15 +0200

    commite me

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -19,11 +19,12 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
                 <div className="display-flex justify-start w-full mb-6 p-2">
 
                 <a href= {`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
-                <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p></a> 
+                    <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p> 
+                </a>
 
-                 <a href= {`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
-                <p className="text-white text-base">To: {shortenAddress(addressTo)}</p></a> 
-
+                <a href= {`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+                    <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
+                </a> 
 
                 </div>
 
