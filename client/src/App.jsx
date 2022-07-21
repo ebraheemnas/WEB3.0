143ac6b8211483923ae96ae7156628042d73f9a6
ebraheemnas <ebraheem.nas@gmail.com>
2022-07-21 22:25:10 +0200

    7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Navbar, Transactions, Loader, Footer, Services, Welcome } from "./components";
 
 
+
+
 const App = () => {
 
   return (
@@ -9,7 +11,10 @@ const App = () => {
           <div className = "gradient-bg-welcome">
 
             <Navbar/>
+
             <Welcome/>
+            
+
 
           </div>
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 import {useState} from 'react';
 import { HiLibrary, HiMenuAlt4, HiOutlineTrendingUp } from 'react-icons/hi';
 import { AiOutlineClose, AiTwotonePicture } from 'react-icons/ai';
-
+import React from 'react';
 
 import logo from '../../images/logo.png';
 
@@ -23,24 +23,26 @@ const Navbar = () => {
     
     return (
         
-        <nav className = "w-full flex md:justify-center justify-between items-center p-4">
+        <nav className = "w-full flex md:justify-center justify-between items-center p-4 nav ">
             <div className = "md:flex-[0.5] flex-initial justify-center items-center">
-
-       
                 <img src={logo} alt="logo" className = "w-32 cursor-pointer" /> 
-           
-
-
             </div>
 
 
-
-
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-              {["About us", "Exchange", "Market", "Install Metamask"].map((item, index) => (
-               <NavbarItem key={item + index} title={item} />
+                <li>
+                    <a href="https://express.adobe.com/page/caHsboaCnaNAI/">About us </a>
+                </li>
+            </ul>
 
+
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial ">
+              {[ "Exchange", "Market", "Install Metamask"].map((item, index) => (
+               <NavbarItem key={item + index} title={item} />
               ))} {/*hoon lazim arj3 wazabet el wade3 3ala 7asab shu ana b7taj lal mwke33    */}
+
+
+
 
                 <li className= "cursor-pointer ext-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">
                     Login 
