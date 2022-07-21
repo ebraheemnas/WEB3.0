7d88e704a50ab4cf192e261c6f88205b4b32f899
ebraheemnas <ebraheem.nas@gmail.com>
2022-07-21 17:47:24 +0200

    4

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -27,7 +27,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 const Welcome = () => {
 
    
-    const {connectWallet, currentAccount, formData, sendTransaction, handleChange} = useContext(TransactionContext);
+    const {connectWallet, currentAccount, formData, sendTransaction, handleChange, isLoading} = useContext(TransactionContext);
 
     
     const handleSubmit =(e) =>{
@@ -137,7 +137,7 @@ const Welcome = () => {
 
                         <div className="h-[1px] w-full bg-gray-400 my-2"/>
 
-                        {false ? (
+                        {isLoading ? (
 
                             <Loader/>
                         ): (
