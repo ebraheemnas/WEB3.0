049edc46dad54c58dcbb078982bf55bee7403066
ebraheemnas <ebraheem.nas@gmail.com>
2022-07-22 20:47:32 +0200

    9

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 
-import {useState} from 'react';
+import {useState , useContext} from 'react';
 import { HiLibrary, HiMenuAlt4, HiOutlineTrendingUp } from 'react-icons/hi';
 import { AiOutlineClose, AiTwotonePicture } from 'react-icons/ai';
 import React from 'react';
 import logo from '../../images/logo.png';
+
+
+
+
 
 
 
@@ -20,6 +24,13 @@ const NavbarItem = ({title, classprops}) =>{
 const Navbar = () => {
 
     const [toggleMenu, setToggleMenu] = useState(false);
+
+
+
+        
+                
+
+    
     
     return (
         
@@ -27,8 +38,6 @@ const Navbar = () => {
             <div className = "md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className = "w-32 cursor-pointer" /> 
             </div>
-
-
 
 
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial ">
@@ -44,9 +53,12 @@ const Navbar = () => {
                 <NavbarItem key={item + index} title={item} />
                 ))}
 
+               
+
 
 
             </ul>
+
             <div className = "flex relative">
                 {toggleMenu
                 ? <AiOutlineClose fontSize={28} className = "text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/>
@@ -80,6 +92,7 @@ const Navbar = () => {
 
         </nav>
     );
+                        
 }
 
 export default Navbar; 
